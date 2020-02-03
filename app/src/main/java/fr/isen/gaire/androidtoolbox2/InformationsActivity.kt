65c17d63baa062f041fa91f1086c02fc162b29f1
Luciegaire <lucie83140@hotmail.fr>
2020-02-03 20:35:27 +0300

    creation information

---
+++ app/src/main/java/fr/isen/gaire/androidtoolbox2/InformationsActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.gaire.androidtoolbox2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informations)
+    }
+}
