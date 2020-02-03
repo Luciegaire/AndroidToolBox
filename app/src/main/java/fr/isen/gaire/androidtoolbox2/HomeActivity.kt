7728a7bde52355341035d5bfd3968b1266a6946b
Luciegaire <lucie83140@hotmail.fr>
2020-02-03 19:17:06 +0300

    form ok

--- app/src/main/java/fr/isen/gaire/androidtoolbox2/HomeActivity.kt
+++ app/src/main/java/fr/isen/gaire/androidtoolbox2/HomeActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.gaire.androidtoolbox2
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,8 +12,20 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val sharedPreferences = getSharedPreferences(Constants.UserPreferencesName, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
         lifeCycleButton.setOnClickListener {
             startActivity(Intent(this, LifeCycleActivity::class.java))
+        }
+
+        logoutbutton.setOnClickListener {
+            editor.clear().commit()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        saveButton.setOnClickListener {
+            startActivity(Intent(this, FormActivity::class.java))
         }
     }
 
